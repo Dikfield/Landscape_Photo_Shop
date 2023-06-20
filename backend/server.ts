853d787d9be3ede82f3eb542ab2964f9f15e22e4
Diogo <dinash@outlook.com>
2023-06-20 16:04:43 +0200

    minor bugs correction

--- backend/server.ts
+++ backend/server.ts
@@ -20,10 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -33,11 +29,11 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: config.PAYPAL_CLIENT_ID }),
 );
 
-if (config.NODE_ENV === 'production') {
+if (config.NODE_ENV as string === 'production') {
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
+  
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
   );
@@ -51,4 +47,6 @@ if (config.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running on port ${port} Node mode ${config.NODE_ENV}`),
+);
