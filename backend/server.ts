c46dbfceca19e6ef68f9e21dffa74bb9e7997510
Diogo <dinash@outlook.com>
2023-07-12 18:00:35 +0200

    correcting path

--- backend/server.ts
+++ backend/server.ts
@@ -32,7 +32,7 @@ app.get('/api/config/paypal', (req, res) =>
 if (config.NODE_ENV as string === 'production') {
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
-  app.use('/watermark', express.static('/var/data/watermark'));
+  app.use('/watermark/uploads', express.static('/var/data/watermark/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   
   app.get('*', (req, res) =>
